0f83f1dc4f4d8c3e3368981ca1c8a461ec2597f5
Sahil Singh Tomar <sahilsinghtomar14435@gmail.com>
2023-08-12 22:56:50 +0530

    some bugs fixed

--- src/components/Houses.js
+++ src/components/Houses.js
@@ -1,15 +1,10 @@
-
-import housesData from "../data"
-
 const houseContainer = {
-    display: "grid",
-    gridTemplateColumns: "1fr 1fr 1fr",
-    gridRowGap: "5rem",
-    gridColumnGap: "5rem"
-}
+  display: 'grid',
+  gridTemplateColumns: '1fr 1fr 1fr',
+  gridRowGap: '5rem',
+  gridColumnGap: '5rem',
+};
 
-export default function Houses({children}) {
-    return <div style={houseContainer}>
-        {children}
-    </div>
+export default function Houses({ children }) {
+  return <div style={houseContainer}>{children}</div>;
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,30 @@
-import "./navbar.css"
+import './navbar.css';
 
 export default function Navbar() {
-    return <nav className="navbar">
-        <h1 className="navbar__heading">Estaery</h1>
-        <ul className="navbar__list list">
-            <li className="navbar__list-el"><a href="">Rent</a></li>
-            <li className="navbar__list-el"><a href="">Buy</a></li>
-            <li className="navbar__list-el"><a href="">Sell</a></li>
-            <li className="navbar__list-el"><a href="">Manage Property</a></li>
-            <li className="navbar__list-el"><a href="">Resources</a></li>
-        </ul>
-        <div className="navbar__action-btn">
-            <button className="login--button">Login</button>
-            <button className="signup--button">Signup</button>
-        </div>
+  return (
+    <nav className="navbar">
+      <h1 className="navbar__heading">Estaery</h1>
+      <ul className="navbar__list list">
+        <li className="navbar__list-el">
+          <a href="#">Rent</a>
+        </li>
+        <li className="navbar__list-el">
+          <a href="#">Buy</a>
+        </li>
+        <li className="navbar__list-el">
+          <a href="#">Sell</a>
+        </li>
+        <li className="navbar__list-el">
+          <a href="#">Manage Property</a>
+        </li>
+        <li className="navbar__list-el">
+          <a href="#">Resources</a>
+        </li>
+      </ul>
+      <div className="navbar__action-btn">
+        <button className="login--button">Login</button>
+        <button className="signup--button">Signup</button>
+      </div>
     </nav>
+  );
 }
