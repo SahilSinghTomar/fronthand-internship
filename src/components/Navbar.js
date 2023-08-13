137ecff2765a7aa8f7079edd032a8f86d8278877
Sahil Singh Tomar <sahilsinghtomar14435@gmail.com>
2023-08-13 06:22:05 +0530

    removed a tag

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,21 +5,11 @@ export default function Navbar() {
     <nav className="navbar">
       <h1 className="navbar__heading">Estaery</h1>
       <ul className="navbar__list list">
-        <li className="navbar__list-el">
-          <a href="#">Rent</a>
-        </li>
-        <li className="navbar__list-el">
-          <a href="#">Buy</a>
-        </li>
-        <li className="navbar__list-el">
-          <a href="#">Sell</a>
-        </li>
-        <li className="navbar__list-el">
-          <a href="#">Manage Property</a>
-        </li>
-        <li className="navbar__list-el">
-          <a href="#">Resources</a>
-        </li>
+        <li className="navbar__list-el">Rent</li>
+        <li className="navbar__list-el">Buy</li>
+        <li className="navbar__list-el">Sell</li>
+        <li className="navbar__list-el">Manage Property</li>
+        <li className="navbar__list-el">Resources</li>
       </ul>
       <div className="navbar__action-btn">
         <button className="login--button">Login</button>
